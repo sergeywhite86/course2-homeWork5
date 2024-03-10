4a48c3a844d627c41977058499faf2c17caddad7
Sergey White <sergey_white86@mail.ru>
2024-03-10 21:34:04 +0300

    обработку исключений вынес в main,добавил условие при невалидных символах в логине или пароле или теперь выбрасывает исключение

--- src/homework5/security/Authorization.java
+++ src/homework5/security/Authorization.java
@@ -4,30 +4,18 @@ import homework5.exceptions.WrongLoginException;
 import homework5.exceptions.WrongPasswordException;
 
 public class Authorization {
-    public boolean auth(String login, String password, String confirmPassword) {
-        try {
-            loginValidation(login);
-            passwordValidation(password);
-            passwordValidationEquals(password, confirmPassword);
-        } catch (WrongLoginException ex) {
-            System.out.println(ex.getMessage());
-            return false;
-        } catch (WrongPasswordException ex) {
-            System.out.println(ex.getMessage());
-            return false;
-        }
-        return login.matches("\\w+") && password.matches("\\w+");
+    public void auth(String login, String password, String confirmPassword)
+            throws WrongPasswordException, WrongLoginException {
+        validation(login, password, confirmPassword);
+        System.out.println("Все данные корректы,поздравляю!");
     }
 
-    private void loginValidation(String login) {
-        if (login.length() >= 20) throw new WrongLoginException();
+    private void validation(String login, String password, String confirmPassword) {
+        if (login.length() >= 20 || !login.matches("\\w+")) throw new WrongLoginException();
+        if (password.length() > 20 || !password.matches("\\w+") || !password.equals(confirmPassword))
+            throw new WrongPasswordException();
     }
 
-    private void passwordValidation(String password) {
-        if (password.length() > 20) throw new WrongPasswordException();
-    }
-
-    private void passwordValidationEquals(String password, String confirmPassword) {
-        if (!password.equals(confirmPassword)) throw new WrongPasswordException();
-    }
 }
+
+
